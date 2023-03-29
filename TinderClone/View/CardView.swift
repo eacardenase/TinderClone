a5223e7b6b6ef346d137ca678a65fed642575711
Edwin Cardenas <eacardenase@unal.edu.co>
2023-03-28 21:11:59 -0500

    feat: add swap functionality

--- TinderClone/View/CardView.swift
+++ TinderClone/View/CardView.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+enum SwipeDirection: Int {
+    case left = -1
+    case right = 1
+}
+
 class CardView: UIView {
     
     // MARK: - Properties
@@ -122,25 +127,51 @@ extension CardView {
         addGestureRecognizer(pan)
         addGestureRecognizer(tap)
     }
+    
+    func panCard(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: nil)
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        let rotationalTransform = CGAffineTransform(rotationAngle: angle)
+        
+        self.transform = rotationalTransform.translatedBy(x: translation.x, y: translation.y)
+    }
+    
+    func resetCardPosition(_ sender: UIPanGestureRecognizer) {
+        
+        let direction: SwipeDirection = sender.translation(in: nil).x > 100 ? .right : .left
+        let shouldDismissCard = abs(sender.translation(in: nil).x) > 120
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            
+            if shouldDismissCard {
+                let xTranslation = CGFloat(direction.rawValue) * 1000
+                let offScreenTransform = self.transform.translatedBy(x: xTranslation, y: 0)
+                
+                self.transform = offScreenTransform
+            } else {
+                self.transform = .identity
+            }
+        }) { _ in
+            if shouldDismissCard {
+                self.removeFromSuperview()
+            }
+        }
+    }
 }
 
 // MARK: - Actions
 
 extension CardView {
     @objc func handlePanGesture(_ sender: UIPanGestureRecognizer) {
-        let translation = sender.translation(in: nil)
         
         switch sender.state {
         case .began:
-            print("DEBUG: Pan did begin")
+            superview?.subviews.forEach({ $0.layer.removeAllAnimations() })
         case .changed:
-            let degrees: CGFloat = translation.x / 20
-            let angle = degrees * .pi / 180
-            let rotationalTransform = CGAffineTransform(rotationAngle: angle)
-            
-            self.transform = rotationalTransform.translatedBy(x: translation.x, y: translation.y)
+            panCard(sender)
         case .ended:
-            print("DEBUG: Pad ended")
+            resetCardPosition(sender)
         default: break
         }
     }
