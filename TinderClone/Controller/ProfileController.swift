cd272d91273a29dbc2cc10a372f6fae673280630
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-06 16:59:26 -0500

    feat: setup collection view on profile controller

--- TinderClone/Controller/ProfileController.swift
+++ TinderClone/Controller/ProfileController.swift
@@ -13,6 +13,32 @@ class ProfileController: UIViewController {
     
     private let user: User
     
+    private lazy var collectionView: UICollectionView = {
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width + 100)
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.scrollDirection = .horizontal
+        
+        let collection = UICollectionView(frame: frame, collectionViewLayout: layout)
+        
+        collection.delegate = self
+        collection.dataSource = self
+        collection.isPagingEnabled = true
+        collection.showsHorizontalScrollIndicator = false
+        collection.register(ProfileCell.self, forCellWithReuseIdentifier: ProfileCell.reuseIdentifier)
+        
+        return collection
+    }()
+    
+    private let dismissButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        button.setImage(UIImage(named: "dismiss_down_arrow")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     init(user: User) {
@@ -32,8 +58,74 @@ class ProfileController: UIViewController {
     }
 }
 
+// MARK: - Helpers
+
 extension ProfileController {
     private func configureUI() {
         view.backgroundColor = .white
+        
+//        collectionView.translatesAutoresizingMaskIntoConstraints = false // we don't use it in order to position it at default values (0, 0)
+        dismissButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(collectionView)
+        view.addSubview(dismissButton)
+
+        // dismissButton
+        NSLayoutConstraint.activate([
+            dismissButton.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: -20),
+            dismissButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            dismissButton.heightAnchor.constraint(equalToConstant: 40),
+            dismissButton.widthAnchor.constraint(equalTo: dismissButton.heightAnchor)
+        ])
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ProfileController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return user.imageURLs.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileCell.reuseIdentifier, for: indexPath)
+        
+        if indexPath.row == 0 {
+            cell.backgroundColor = .red
+        } else {
+            cell.backgroundColor = .blue
+        }
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension ProfileController: UICollectionViewDelegate {
+    
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension ProfileController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.width + 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+// MARK: - Actions
+
+extension ProfileController {
+    @objc func handleDismissal() {
+        dismiss(animated: true)
     }
 }
